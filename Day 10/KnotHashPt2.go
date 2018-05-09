da67d8805359bfb9174b3c18089f6e7c43399dd3
Nick Staggs <nick.c.staggs@gmail.com>
2018-05-09 06:44:55 -0400

    Day 10 pt 2

--- Day 10/KnotHashPt2.go
+++ Day 10/KnotHashPt2.go
@@ -26,6 +26,8 @@ func main() {
 	}
 
 	byteArr := []byte(line)
+	addedBytes := []byte{17, 31, 73, 47, 23}
+	byteArr = append(byteArr, addedBytes...)
 
 	arr := make([]int, size)
 	for i := 0; i < size; i++ {
@@ -33,17 +35,32 @@ func main() {
 	}
 
 	curr := 0
-	for skip, l := range byteArr {
-		end := curr + int(l) - 1
-		fmt.Println(curr, end)
-		for j := curr; j < end; j++ {
-			arr[j%size], arr[end%size] = arr[end%size], arr[j%size]
-			end--
-		}
+	skip := 0
+	for i := 0; i < 64; i++ {
+		for _, l := range byteArr {
+			end := curr + int(l) - 1
+			for j := curr; j < end; j++ {
+				arr[j%size], arr[end%size] = arr[end%size], arr[j%size]
+				end--
+			}
 
-		curr = (curr + skip + int(l)) % size
+			curr = (curr + skip + int(l)) % size
+			skip++
+		}
 	}
 
-	fmt.Println("The multiplication of the first two numbers equals", arr[0]*arr[1])
-	fmt.Println(arr)
+	var denseHash []int
+	var denseHashHex []string
+	for i := 0; i < 16; i++ {
+		num := arr[i*16]
+		for j := 1; j < 16; j++ {
+			num = num ^ arr[i*16+j]
+		}
+		denseHash = append(denseHash, num)
+		denseHashHex = append(denseHashHex, fmt.Sprintf("%x", num))
+	}
+
+	// 0s wont be printed if leading aka int 9 will yield the hex "9" not "09"
+	fmt.Println(denseHash)
+	fmt.Println(denseHashHex)
 }
